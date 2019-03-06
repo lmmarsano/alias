03e4431b8b74bbad29f5c28c2cdbd172f7ad2310
Luis Marsano <luis.marsano@gmail.com>
2019-03-06 01:41:16 -0500

    define Command types
    define ICommand interface & implementations to run commands

---
+++ source/Alias/ExternalCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SCG = System.Collections.Generic;
+
+namespace Alias {
+	/**
+	 * <summary>
+	 * An external command built from a base command and additional arguments.
+	 * </summary>
+	 */
+	class ExternalCommand: ICommand {
+		/**
+		 * <summary>
+		 * Additional arguments.
+		 * </summary>
+		 * <value>Arguments to append to a base command.</value>
+		 */
+		public SCG.IEnumerable<string> Arguments { get; }
+		/**
+		 * <summary>
+		 * An base command to run externally.
+		 * </summary>
+		 * <value>Command string matching native console conventions.</value>
+		 */
+		public string BaseCommand { get; }
+		/**
+		 * <summary>
+		 * Run external command: run the command string as is with appended arguments.
+		 * </summary>
+		 * <returns>The external command’s exit code.</returns>
+		 */
+		public ExitCode Run() {
+			throw new NotImplementedException();
+		}
+		/**
+		 * <summary>
+		 * Construct an external command from a base command and arguments.
+		 * </summary>
+		 * <param name="arguments">Additional arguments following the base command.</param>
+		 * <param name="baseCommand">A command string.</param>
+		 */
+		public ExternalCommand(SCG.IEnumerable<string> arguments, string baseCommand) {
+			Arguments = arguments;
+			BaseCommand = baseCommand;
+		}
+	}
+}
