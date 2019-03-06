0a7547ba9f4ee62bc1293d7a8e04d781d4fe3e8f
Luis Marsano <luis.marsano@gmail.com>
2019-03-06 01:40:08 -0500

    add Exception types
    add Exception types for errors to handle at top-level

--- source/Alias/Exception.cs
+++ source/Alias/Exception.cs
@@ -1,23 +1,65 @@
 ﻿using S = System;
+using SIO = System.IO;
+using SSP = System.Security.Permissions;
 using NJL = Newtonsoft.Json.Linq;
 
 namespace Alias {
 	/**
 	 * <summary>
-	 * Base class for exceptions originating from <c cref='Alias'>Alias</c>.
+	 * Interface for exceptions originating from <c cref='Alias'>Alias</c>.
 	 * </summary>
 	 */
-	public abstract class Exception: S.Exception {
-		public Exception() {}
-		public Exception(string message) : base(message) {}
-		public Exception(string message, S.Exception innerException): base(message, innerException) {}
+	public interface IException {}
+	/**
+	 * <summary>
+	 * Interface for exceptions terminating program.
+	 * </summary>
+	 */
+	interface ITerminalException: IException {}
+	/**
+	 * <summary>
+	 * Error attempting to access file.
+	 * </summary>
+	 */
+	class FileException: S.IO.IOException, ITerminalException {
+		public string FilePath { get; }
+		public SSP.FileIOPermissionAccess Type { get; }
+		public FileException(string filePath, SSP.FileIOPermissionAccess type) {
+			FilePath = filePath;
+			Type = type;
+		}
+		public FileException(string filePath, SSP.FileIOPermissionAccess type, string message): base(message) {
+			FilePath = filePath;
+			Type = type;
+		}
+		public FileException(string filePath, SSP.FileIOPermissionAccess type, string message, S.Exception innerException): base(message, innerException) {
+			FilePath = filePath;
+			Type = type;
+		}
+	}
+	/**
+	 * <summary>
+	 * Error deserializing file.
+	 * </summary>
+	 */
+	class DeserialException: S.IO.IOException, ITerminalException {
+		public SIO.FileInfo File { get; }
+		public DeserialException(SIO.FileInfo file) {
+			File = file;
+		}
+		public DeserialException(SIO.FileInfo file, string message): base(message) {
+			File = file;
+		}
+		public DeserialException(SIO.FileInfo file, string message, S.Exception innerException): base(message, innerException) {
+			File = file;
+		}
 	}
 	/**
 	 * <summary>
 	 * Thrown when execution encounters a <c cref='NJL.JToken'>JToken</c>-derived object the program is unable to process.
 	 * </summary>
 	 */
-	public class UnhandledJsonTokenException: Exception {
+	public class UnhandledJsonTokenException: S.InvalidOperationException, IException {
 		/**
 		 * <summary>A <c cref='NJL.JToken'>JSON token</c> with unhandled runtime type.</summary>
 		 * <value>A <c cref='NJL.JToken'>JSON token</c> the program is unable to process: this object has an unrecognized runtime type.</value>
