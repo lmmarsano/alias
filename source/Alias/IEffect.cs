19df9c76599925bb4f42f4eea31430d234adff5e
Luis Marsano <luis.marsano@gmail.com>
2019-03-21 04:36:09 -0400

    consolidate IEffect
    merge IEffect* interfaces

--- source/Alias/IEffect.cs
+++ source/Alias/IEffect.cs
@@ -1,8 +1,43 @@
+using SIO = System.IO;
+using F = Functional;
+using Command = System.String;
+using Arguments = System.String;
+using WorkingDirectory = System.String;
+
 namespace Alias {
 	/**
 	 * <summary>
 	 * Effects needed to perform operations.
 	 * </summary>
 	 */
-	interface IEffect: IEffectRunCommand, IEffectCreateFile {}
+	interface IEffect {
+		/**
+		 * <summary>
+		 * Copy file to destination.
+		 * </summary>
+		 * <param name="file">File to copy.</param>
+		 * <param name="destination">Destination to copy file to.</param>
+		 * <returns>Result of nothing or error.</returns>
+		 */
+		F.Result<F.Nothing> CopyFile(IFileInfo file, string destination);
+		/**
+		 * <summary>
+		 * Run command from working directory.
+		 * </summary>
+		 * <param name="workingDirectory">New process’s working directory.</param>
+		 * <param name="command">Command for new process.</param>
+		 * <returns>Command’s exit code.</returns>
+		 */
+		F.Result<ExitCode> RunCommand(WorkingDirectory workingDirectory, Command command);
+		/**
+		 * <summary>
+		 * Run command with arguments from working directory.
+		 * </summary>
+		 * <param name="workingDirectory">New process’s working directory.</param>
+		 * <param name="command">Command for new process.</param>
+		 * <param name="arguments">Command’s arguments.</param>
+		 * <returns>Command’s exit code.</returns>
+		 */
+		F.Result<ExitCode> RunCommand(WorkingDirectory workingDirectory, Command command, Arguments arguments);
+	}
 }
